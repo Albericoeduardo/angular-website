14d4da75d5dffd37fa8ee7e0a450eb6672b9b164
Albérico Eduardo <albericoeduardo202@gmail.com>
2023-09-06 10:12:45 -0300

    bug resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar/navbar.component';
-import { CardProfileComponent } from './components/card-profile/card-profile/card-profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardProfileComponent } from './components/card-profile/card-profile.component';
 import { LatestPostCardComponent } from './components/latest-post-card/latest-post-card.component';
 import { UserStatisticsCardComponent } from './components/user-statistics-card/user-statistics-card.component';
 
